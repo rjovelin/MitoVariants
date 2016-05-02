da2e39f6edb7efb60dcf1c923e90bc4b44702aa2
Richard <richard.jovelin@oicr.on.ca>
2016-05-02 13:25:29 -0400

    initial commit

---
+++ PlotAlleleFrequencyFunction.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  2 12:52:51 2016
+
+@author: RJovelin
+"""
+
+# use this script to plot an histogram of allele frequencies for various functional categories
+
+# place this script in folder with heteroplasmy summary files
+
+# usage python3 PlotAlleleFrequencyFunction.py [options]
+# - [singlefile/allfiles]: whether a single summary file or multiple summary files
+# - [frequency/counts]: plot frequency or counts of mutational effects
+# -tumor: summary file of tumor if singlefile is used
+# - outputfile
+
+import os
+import sys
+import matplotlib.pyplot as plt
+from matplotlib import patches as mpatches
+import numpy as np
+from mito_mutations import *
+
+
+# make a list of summary files 
+files = [i for i in os.listdir() if 'RNAOnly' in i and '.txt' in i]
+
+##use single of all summary files
+#which_files = sys.argv[1]
+#datatype = sys.argv[2] 
+#if which_files == 'singlefile':
+#    # get tumor from command
+#    tumor = sys.argv[3]
+#    # create a 1 item list with the summary file
+#    for filename in files:
+#        if tumor in filename:
+#            break
+#    files = [filename]
+#    outputfile = sys.argv[4]
+#elif which_files == 'allfiles':
+#    outputfile = sys.argv[3]    
+
+
+outputfile = 'testfig.pdf'
+Sites = 'mutations'
+frequency = 'frequency'
+which_files = 'allfiles'
+
+# create a dict {mutation: [list of positions]}
+mutations = {}
+
+# loop over filename in files
+for filename in files:
+    # create a dict {mutation: [list of positions]}
+    snpeffect = ComputeMAFFunction(filename)
+    # populate mutation dict
+    for effect in snpeffect:
+        # copy list of frequencies
+        freq = list(snpeffect[effect])
+        # add poitions to list value
+        if effect in mutations:
+            mutations[effect].extend(freq)
+        else:
+            mutations[effect] = freq
+
+categories = [i for i in mutations if i != 'NA']
+print(categories)
+snps = []
+for i in mutations:
+    if i != 'NA':
+        snps.append(mutations[i])
+plt.hist(snps, label=categories)        
+        
+#plt.hist(snps, label = categories)
+
+plt.show()
+
+var = {}
+for i in mutations:
+    if i != 'NA':
+        for j in mutations[i]:
+            if j < 0.1:
+                if i in var:
+                    var[i].append(j)
+                else:
+                    var[i] = [j]
+muts = []
+for i in var:
+    muts.append([len(var[i]), i])
+muts.sort()
+print(muts)
+
+
+# add a title
+# add a label
+# order freqs in decreasing order of the first freq bin
+# change y and x ticks
+# opetion to have frequencies instead of counts
+
+
+            
+## make a list of mutation names sorted by count
+#name_counts = []
+#for i in mutations:
+#    if i in ['DLoop', 'non-synonymous', 'stoploss', 'stopgain', 'synonymous', 'NonCoding', 'tRNA']:
+#        if i == 'non-synonymous':
+#            name_counts.append([len(mutations[i]), 'NonSyn'])
+#        elif i == 'synonymous':
+#            name_counts.append([len(mutations[i]), 'Syn'])
+#        elif i == 'stoploss':
+#            name_counts.append([len(mutations[i]), 'SCL'])
+#        elif i == 'stopgain':
+#            name_counts.append([len(mutations[i]), 'PSC'])
+#        else:
+#            name_counts.append([len(mutations[i]), i])
+#name_counts.sort()
+#categories = []
+#for i in name_counts:
+#    categories.append(i[1])
+#    
+#counts = [i[0] for i in name_counts]
+#print(categories)
+#print(counts)
+#
+## create figure
+#fig = plt.figure(1, figsize = (4.3,2.56))
+#
+## add axe to fig
+#ax = fig.add_subplot(1, 1, 1)
+#
+## Set the bar width
+#bar_width = 0.4
+#
+## set positions of the left bar-boundaries
+#bar_left = [i for i in range(len(counts))]
+## set positions of the x-axis ticks (center of the bars as bar labels)
+#tick_pos = [i+(bar_width/2) for i in bar_left]
+#
+## Create a bar plot, in position bar_left for counts
+#plt.bar(bar_left, counts, width=bar_width, color= 'red')
+#
+## set the x ticks with names
+#plt.xticks(tick_pos, categories, size = 12)
+#
+##### need to edit the y ticks
+#
+#if frequency == 'frequency':
+#    # count total number of mutations
+#    total = sum(counts)
+#    print(total)
+#    # set the y ticks
+#    counts = list(map(lambda x: x / total, counts))
+#    plt.yticks([i/100 for i in range(0, 125, 25)], [0, 0.25, 0.50, 0.75, 1])
+#
+## set axis labels
+#if frequency == 'frequency':
+#    plt.ylabel('Proportion of mutations', size = 12, ha = 'center', fontname = 'Helvetica', family = 'sans-serif', color = 'black')
+#elif frequency == 'counts':
+#    plt.ylabel('Number of mutations', size = 12, ha = 'center', fontname = 'Helvetica', family = 'sans-serif', color = 'black')
+#
+#plt.xlabel('Mutational effect', size = 12, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
+#
+## Set a buffer around the edge
+#plt.xlim([min(tick_pos)-bar_width, max(tick_pos)+bar_width])
+#
+## add a light grey horizontal grid to the plot, semi-transparent, 
+#ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)  
+## hide these grids behind plot objects
+#ax.set_axisbelow(True)
+#
+#
+#plt.margins()
+#  
+## do not show lines around figure  
+#ax.spines["top"].set_visible(False)    
+#ax.spines["bottom"].set_visible(False)    
+#ax.spines["right"].set_visible(False)    
+#ax.spines["left"].set_visible(False)      
+#
+#if which_files == 'singlefile':
+#    plt.title(tumor, size = 12)
+#elif which_files == 'allfiles':
+#    plt.title('All tumor types', size = 12)
+#
+#plt.tick_params(
+#    axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
+#    which='both',      # both major and minor ticks are affected
+#    bottom='off',      # ticks along the bottom edge are off
+#    top='off',         # ticks along the top edge are off
+#    right = 'off',
+#    left = 'off',          
+#    labelbottom='on', # labels along the bottom edge are off 
+#    colors = 'black'
+#    )  
+
+# save figure
+fig.savefig(outputfile, bbox_inches = 'tight')
+    
