23642ba6d2e7904bd211d67b49d82ed89851506f
Richard <richard.jovelin@oicr.on.ca>
2016-06-13 13:54:52 -0400

    simplified parameters and run on server

--- Plot_Polymorphism.py
+++ Plot_Polymorphism.py
@@ -5,8 +5,18 @@ Created on Tue Feb 23 15:18:49 2016
 @author: RJovelin
 """
 
+
+
+# import matplotlib and change api to use on server
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib import rc
+rc('mathtext', default='regular')
+# import built in modules
 import sys
+# import custom modules
 from mito_mutations import *
 
 
@@ -17,26 +27,29 @@ from mito_mutations import *
 # usage python3 Plot_Polymorphism.py [parameters]
 # - heteroplasmysummaryfile: file with heteroplasmic positions in the population sample
 # - threshold: the value above which PIC is considered (eg. threshold = 0 means 0 values won't be shown)
-# - [normal/cancer/specific] whether the sample is germline or tumor or SNPs are tumor-specific
 # - [full/limited/black] : color panel: each gene has a different color (full) 
 #                        or proteins are in grey and tRNAs in red (limited)
 #                        or only tRNAs are labeled in red (black)
-# - tumor: tumor name
-# - outputfile: save fig to outputfile
 
 
 # get file with heteroplasmic positions and read counts
 HeteroSummaryFile = sys.argv[1]
 # get thresold from command
 threshold = float(sys.argv[2])
-# get normal or cancer from command
-sample = sys.argv[3]
 # get the color panel to use
-colors = sys.argv[4]
-# get the tumor name
-tumor = sys.argv[5]
-# get outputfile from command
-outputfile = sys.argv[6]
+colors = sys.argv[3]
+
+# extract information about sample type from file name [normal/tumor/specific]
+# whether the sample is germline or tumor or SNPs are tumor-specific
+if 'TumorSpecific' in HeteroSummaryFile:
+    sample = 'specific'
+elif 'tumor' in HeteroSummaryFile:
+    sample = 'tumor'
+elif 'normal' in HeteroSummaryFile:
+    sample = 'normal'
+
+# get cancer name from summary file
+cancer = HeteroSummaryFile[HeteroSummaryFile.index('_') + 1: HeteroSummaryFile.index('_' + sample)]
 
 # get the positions of each mitochiondrial gene
 mito_genes = MitoAnnotation('rCRS_genes_MT.text.txt')
@@ -51,19 +64,20 @@ positions = [i for i in polymorphism]
 # create a dict with gene : color
 if colors == 'full':
     # tRNAs are in red, each portein coding gene has a different color    
-    gene_colors = {'ATP6': [1, 0.988, 0], 'ATP8': [0.78, 0.24, 0.431],
-                   'COX1': [1, 0.757, 0.212], 'COX2': [0.12, 0.557, 0.682], 
-                   'COX3': [0.145, 0.529, 0], 'CYTB': [0.145, 0.51, 0.745],
-                   'ND1': [1, 0.843, 0], 'ND2': [1, 0.443, 0],
-                   'ND3': [0.576, 0.12, 0.718], 'ND4': [0.239, 0.878, 0],
-                   'ND4L': [0.329, 0.004, 0.408], 'ND5': [0.875, 0, 0.384],
-                   'ND6': [1, 0.69, 0], 'RNR1': [0, 0.741, 0.424],
-                   'RNR2': [0, 0.427, 0.243], 'TRNA': 'red',
+    gene_colors = {'ATP6': '#8dd3c7', 'ATP8': '#bc80bd',
+                   'COX1': '#ccebc5', 'COX2': '#fdb462', 
+                   'COX3': '#b3de69', 'CYTB': '#fccde5',
+                   'ND1': '#ffffb3', 'ND2': '#ffed6f',
+                   'ND3': '#fb8072', 'ND4': '#80b1d3',
+                   'ND4L': '#80b1d3', 'ND5': '#b3de69',
+                   'ND6': '#fdb462', 'RNR1': '#d9d9d9',
+                   'RNR2': '#bebada', 'TRNA': 'red',
                    'TRNC': 'red', 'TRND':'red', 'TRNE': 'red', 'TRNF': 'red', 'TRNG': 'red',
                    'TRNH': 'red', 'TRNI': 'red', 'TRNK': 'red', 'TRNL1': 'red', 'TRNL2': 'red',
                    'TRNM': 'red', 'TRNN': 'red', 'TRNP': 'red', 'TRNQ': 'red', 'TRNR': 'red',
                    'TRNS1': 'red', 'TRNS2':'red', 'TRNT': 'red', 'TRNV': 'red', 'TRNW': 'red',
                    'TRNY': 'red'}
+
 elif colors == 'black':
     # tRNAs are in red all others are in black
     gene_colors = {'ATP6': 'black', 'ATP8': 'black', 'COX1': 'black', 'COX2': 'black',
@@ -115,11 +129,11 @@ ax.set_ylim([0, 1])
             
 # set title
 if sample == 'normal':
-    ax.set_title('{0} - Heteroplasmies in germline\n'.format(tumor), size = 10, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
+    ax.set_title('{0} - Heteroplasmies in germline\n'.format(cancer), size = 10, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
 elif sample == 'cancer':
-    ax.set_title('{0} - Heteroplasmies in tumor\n'.format(tumor), size = 10, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
+    ax.set_title('{0} - Heteroplasmies in tumor\n'.format(cancer), size = 10, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
 elif sample == 'specific':
-    ax.set_title('{0} - Tumor-specific heteroplasmies\n'.format(tumor), size = 10, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
+    ax.set_title('{0} - Tumor-specific heteroplasmies\n'.format(cancer), size = 10, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
 
 # set y axis label
 ax.set_ylabel('Polymorphism Information Content', size = 10, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
@@ -160,13 +174,9 @@ plt.tick_params(
     labelbottom='off') # labels along the bottom edge are off  
   
   
- 
+# build outputfile with parameters
+outputfile = 'Polymorphism' + cancer + sample + '.pdf' 
   
 # save figure
 fig.savefig(outputfile, bbox_inches = 'tight')
-
-
-
-
-
 
