14b6fa2752b311f542d2a4e6f369b3fb1481afd5
Richard <richard.jovelin@oicr.on.ca>
2016-07-06 14:32:03 -0400

    sort allele frequencies according to MRPP3 genotype

--- PlotCorrelMRPP3GenotypeAltFreq.py
+++ PlotCorrelMRPP3GenotypeAltFreq.py
@@ -10,7 +10,6 @@ Created on Tue Jul  5 11:54:09 2016
 # usage PlotCorrelMRPP3GenotypeAltFreq [options]
 # -[trna/P9/allgenes]: consider only RNA modifications in tRNA, modifs in tRNA P9 or all genes
 
-
 # place this script in folder HeteroPlasmyFilesRNAOnly with summary files
 
 # import matplotlib and change api to use on server
@@ -29,6 +28,12 @@ from scipy import stats
 from mito_mutations import *
 
 
+# fix threshold to detect heteroplasmy to 1%
+threshold = 1
+
+# get site type from command
+SiteType = sys.argv[1]
+
 
 # create a dict {individual: genotype}
 Genotypes = {}
@@ -40,27 +45,27 @@ VCFTumor = ['COAD', 'OV', 'RECA-EU', 'UCEC', 'CESC', 'LGG', 'LIRI', 'SARC', 'STA
 for folder in VCFTumor:
     # make a list of gzip files    
     if folder == 'STAD':
-        files = [i for i in os.listdir(folder + '/filteredVCFS') if i[-3:] == '.gz']
+        files = [i for i in os.listdir('../../../VCFs/Germlines_vcf/perCancerType/' + folder + '/filteredVCFS') if i[-3:] == '.gz']
     else:
-        files = [i for i in os.listdir(folder) if i[-3:] == '.gz']
+        files = [i for i in os.listdir('../../../VCFs/Germlines_vcf/perCancerType/' + folder) if i[-3:] == '.gz']
     if len(files) != 0:
         # unzip files
         if folder == 'STAD':
             for filename in files:
-                os.system('gunzip ' + folder + '/filteredVCFS/' + filename)
+                os.system('gunzip ' + '../../../VCFs/Germlines_vcf/perCancerType/' + folder + '/filteredVCFS/' + filename)
         else:
             for filename in files:
-                os.system('gunzip ' + folder + '/' + filename)
+                os.system('gunzip ' + '../../../VCFs/Germlines_vcf/perCancerType/' + folder + '/' + filename)
 print('done unzipping VCF files')                
 
 # make a list of WGS normal files with corresponding individual ID and 
-MatchFiles = [i for i in os.listdir() if 'WGS_NTOnly' in i]
+MatchFiles = [i for i in os.listdir('../') if 'WGS_NTOnly' in i]
 assert len(MatchFiles) == 9, 'there should be matching files for 9 tumors'
 
 # create a dict {BAM ID : individual ID} 
 MatchingIDs = {}
 for filename in MatchFiles:
-    infile = open(filename)
+    infile = open('../' + filename)
     for line in infile:
         if line.rstrip() != '':
             line = line.rstrip().split('\t')
@@ -72,45 +77,287 @@ for filename in MatchFiles:
             # populate dict
             MatchingIDs[BamID] = Individual
     infile.close()
-print('matched Bams with Individual IDs')
+print('matched Bams with Individual IDs', len(MatchingIDs))
 
-
-
- 
-
-
-           
-            
-
-
-# find the ID - VCF matchs
-
-# get the genotypes of MRPP3 at rs11156878 
+# loop over folders, extract MRPP3 genotypes of all individuals
+for folder in VCFTumor:
+    # make a list of gzip files    
+    if folder == 'STAD':
+         mrpp3 = GetMRPP3Genotypes('../../../VCFs/Germlines_vcf/perCancerType/' + folder + '/filteredVCFS')
+    else:
+        mrpp3 = GetMRPP3Genotypes('../../../VCFs/Germlines_vcf/perCancerType/' + folder)
+    # populate Genotypes dict with individual ID
+    ID = [i for i in mrpp3]
+    assert len(ID) == 1, 'there should be a single indivual'
+    ID = ID[0]
+    if ID in MatchingIDs:
+        Genotypes[MatchingIDs[ID]] = mrpp3[ID]
+print('extracted MRPP3 genotype', len(Genotypes))                
+    
 
 # get the heteroplasmy frequency for trna, p9 or all genes 
 
+# get the positions of each mitochiondrial gene
+mito_genes = MitoAnnotation('rCRS_genes_MT.text.txt')
+# make a set of tRNA positions
+trna_indices = []
+for gene in mito_genes:
+    if gene.startswith('TRN'):
+        for i in mito_genes[gene]:
+            trna_indices.append(i)
+print('got tRNA indices')
+
+# get the gene coordinates [start, end, orientation]
+mito_coords = MitoCoordinates('rCRS_genes_MT.text.txt')
+print('got gene coordinates')
+
+
+# make a list of summary files 
+SummaryFiles = [i for i in os.listdir() if 'tumor_RNAOnly' in i and '.txt' in i]
+
+# create a dict {mutation: [list of allele frequencies]}
+mutations = {}
+
+# loop over filename in files
+for filename in files:
+    # open file for reading
+    infile = open(filename)
+    infile.readline()
+    for line in infile:
+        line = line.rstrip()
+        if line != '':
+            line = line.split('\t')
+            # grab participant ID
+            participant = line[1]
+            # get position 0-based
+            position = int(line[0]) - 1
+            # get ref allele
+            ref = line[6]
+            # get major and minor allele
+            major, minor = line[7], line[8]
+            # get major and minor counts
+            major_counts, minor_counts = int(line[9]), int(line[10])
+            # get gene
+            gene = line[2]
+            # verify that major and minor read counts > threshold
+            alleles = [major, minor]
+            total_reads = 0
+            allele_freq = []
+            for i in range(12, 20):
+                total_reads += int(line[i])
+            for i in range(len(alleles)):
+                if alleles[i] == 'A':
+                    allele_counts = int(line[12]) + int(line[16])
+                elif alleles[i] == 'T':
+                    allele_counts = int(line[13]) + int(line[17])
+                elif alleles[i] == 'C':
+                    allele_counts = int(line[14]) + int(line[18])
+                elif alleles[i] == 'G':
+                    allele_counts = int(line[15]) + int(line[19])
+                assert (allele_counts / total_reads) * 100 > threshold, 'allele frequency is lower than heteroplasmy threshold'   
+                # check if allele counts is major or minor allele count                
+                if i == 0:
+                    assert allele_counts == major_counts, 'allele count is different than major count'
+                elif i == 1:
+                    assert allele_counts == minor_counts, 'allele count is different than minor count'
+                allele_freq.append(allele_counts / total_reads)
+            # identify mutant allele by comparing minor and major to ref
+            # get the frequency (read count / total reads at position) for the mutant allele
+            if major == ref and minor != ref:
+                # mutant is minor
+                freq = allele_freq[1]
+            elif major != ref and minor == ref:
+                # mutant is major            
+                freq = allele_freq[0]
+            # check which sites need to be recorded
+            if SiteType == 'allgenes':
+                # record all sites
+                if participant in mutations:
+                    mutations[participant].append(freq)
+                else:
+                    mutations[participant] = [freq]
+            elif SiteType == 'trna':
+                # record tRNA sites
+                if line[2].startswith('TRN'):
+                    assert position in trna_indices, 'position should be trna'
+                    # position is in tRNA, populate dict
+                    if participant in mutations:
+                        mutations[participant].append(freq)
+                    else:
+                        mutations[participant]  [freq]
+            elif SiteType == 'P9':
+                assert position in trna_indices, 'position should be a trna index'
+                # record tRNA P9 sites
+                # find the tRNA corresponding to current site
+                for i in mito_genes:
+                    if position in mito_genes[i]:
+                        # stop looking, found tRNA gene
+                        break
+                assert i == gene, 'gene name does not match woth expected position'
+                # convert genomic position to tRNA position
+                assert position in range(mito_coords[gene][0], mito_coords[gene][1]), 'position should be in gene'
+                trnaposition = GenomicPositionToGenePosition(position, mito_coords[gene][0], mito_coords[gene][1], mito_coords[gene][2])
+                # record only P9 posiitons (8 in 0-based index)
+                if trnaposition == 8:
+                    if participant in mutations:
+                        mutations[participant].append(freq)
+                    else:
+                        mutations[participant] = [freq]
+                 
+    infile.close()
+print('got allele frequencies')            
+
+# create lists with frequencies for each geneotype
+AA = [freq for freq in mutations[participant] if Genotypes[participant] == 'AA']
+AG = [freq for freq in mutations[participant] if Genotypes[participant] == 'AG']
+GG = [freq for freq in mutations[participant] if Genotypes[participant] == 'GG']        
+        
+# count the number of participants
+total_participants = 0
+# loop over individual IDs in frequency dict
+for participant in mutations:
+    # check if participant has genotype
+    if participant in Genotypes:
+        # update counter
+        total_participants += 1
+print('total participants', total_participants)
+print('AA', len(AA))
+print('AG', len(AG))
+print('GG', len(GG))
 
 
 
 
-/RQusagers/rjovelin/awadalla_group/TCGA/VCFs/Germlines_vcf/perCancerType
-
-
-/RQusagers/rjovelin/awadalla_group/TCGA/Mitoseek/mitoseek_hp1_mbq20
-
-/RQusagers/rjovelin/awadalla_group/TCGA/Mitoseek/mitoseek_hp1_mbq20/HeteroPlasmyFilesRNAOnly
 
 
 
 
-# create a correlation plot between alternative allele frequency as a function of genotypes at given position
-
-# usage plot_correlation_altfreq_genotype.py [parameters]
-# inputfile : file with genotypes and allele frequencies
-# outputfile : figure file
-# SNP ID: ID of the SNP with genotype
 
 
+# sort frequency values
+for effect in mutations:
+    mutations[effect].sort()
+
+# make list with functional category and data
+Data = [[key, val] for key, val in mutations.items()]
+# replace some functional categories with a shorter name
+for i in range(len(Data)):
+    if Data[i][0] == 'stopgain':
+        Data[i][0] = 'PSC'
+    elif Data[i][0] == 'stoploss':
+        Data[i][0] = 'SCL'
+    elif Data[i][0] == 'non-synonymous':
+        Data[i][0] = 'NonSyn'
+    elif Data[i][0] == 'synonymous':
+        Data[i][0] = 'Syn'
+    # keep the same name for the other categories (tRNA, DLoop, NonCoding, Ribosomal)
+
+# sort according to names
+Data.sort()
+
+# create parallel lists with functional names and data
+categories = [Data[i][0] for i in range(len(Data))]
+data = [Data[i][1] for i in range(len(Data))]    
+print(categories)
+
+
+# create figure
+fig = plt.figure(1, figsize = (3.5, 2.5))
+# add a plot to figure (1 row, 1 column, 1 plot)
+ax = fig.add_subplot(1, 1, 1)  
+
+# create a list to store the variables for building the legend
+Graphs = []
+
+# make a list of colors
+colorscheme = ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5']
+
+# loop over categories
+for i in range(len(categories)):
+    print(categories[i])
+    graph = ax.step(data[i], np.linspace(0, 1, len(data[i]), endpoint=True), linewidth = 1.2, color = colorscheme[i], alpha = 0.7)
+    Graphs.append(graph)
+print('plotted CDF')
+
+# add label for the Y axis
+ax.set_ylabel('Cumulative fraction of mutations', size = 10, ha = 'center', fontname = 'Arial')
+# set x axis label
+ax.set_xlabel('Mutant allele Frequency', size = 10, ha = 'center', fontname = 'Arial')
+
+# do not show lines around figure, keep bottow line  
+ax.spines["top"].set_visible(False)    
+ax.spines["bottom"].set_visible(True)    
+ax.spines["right"].set_visible(False)    
+ax.spines["left"].set_visible(False)      
+# offset the spines
+for spine in ax.spines.values():
+  spine.set_position(('outward', 5))
+
+# add a light grey horizontal grid to the plot, semi-transparent, 
+ax.yaxis.grid(True, linestyle='--', which='major', color='lightgrey', alpha=0.5, linewidth = 0.5)  
+# hide these grids behind plot objects
+ax.set_axisbelow(True)
+
+# do not show ticks on 1st graph
+ax.tick_params(
+    axis='x',       # changes apply to the x-axis and y-axis (other option : x, y)
+    which='both',      # both major and minor ticks are affected
+    bottom='on',      # ticks along the bottom edge are off
+    top='off',         # ticks along the top edge are off
+    right = 'off',
+    left = 'off',          
+    labelbottom='on', # labels along the bottom edge are off 
+    colors = 'black',
+    labelsize = 10,
+    direction = 'out') # ticks are outside the frame when bottom = 'on
+
+# do not show ticks
+ax.tick_params(
+    axis='y',       # changes apply to the x-axis and y-axis (other option : x, y)
+    which='both',      # both major and minor ticks are affected
+    bottom='off',      # ticks along the bottom edge are off
+    top='off',         # ticks along the top edge are off
+    right = 'off',
+    left = 'off',          
+    labelbottom='off', # labels along the bottom edge are off 
+    colors = 'black',
+    labelsize = 10,
+    direction = 'out') # ticks are outside the frame when bottom = 'on
+
+for label in ax.get_yticklabels():
+    label.set_fontname('Arial')
+
+# add lines
+lns = Graphs[0]
+for i in range(1, len(Graphs)):
+    lns = lns + Graphs[i]
+# get labels
+labs = [categories[i] for i in range(len(categories))]
+print(labs)
+# plot legend
+ax.legend(lns, labs, loc=4, fontsize = 6, frameon = False)
+
+# build outputfile namewith parameters
+# extract the cancer name
+if which_files == 'singlefile' and sample == 'tumor':
+    cancer = HeteroSummaryFile[HeteroSummaryFile.index('_') + 1: HeteroSummaryFile.index('_' + sample)]
+elif which_files == 'singlefile' and sample == 'specific':
+    cancer = HeteroSummaryFile[HeteroSummaryFile.index('_') + 1: HeteroSummaryFile.index('_TumorSpecific')]
+
+if which_files == 'singlefile':
+    outputfile = 'CDFMutantAlleleFreq' + cancer + sample.capitalize() + '.pdf'
+elif which_files == 'allfiles':
+    outputfile = 'CDFMutantAllelefreq' + sample.capitalize() + '.pdf'
+
+fig.savefig(outputfile, bbox_inches = 'tight')
+
+
+
+
+
+
+
+#####################################
 
 
 
