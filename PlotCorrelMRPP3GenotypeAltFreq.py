27a384111620b88abd1a43a12199602f92513c4f
Richard <richard.jovelin@oicr.on.ca>
2016-07-06 15:40:33 -0400

    plot allele frequency for each MRPP3 genotype

--- PlotCorrelMRPP3GenotypeAltFreq.py
+++ PlotCorrelMRPP3GenotypeAltFreq.py
@@ -226,93 +226,49 @@ print('AG', len(AG))
 print('GG', len(GG))
 
 
-
-
-
-
-
-
-
-
-# sort frequency values
-for effect in mutations:
-    mutations[effect].sort()
-
-# make list with functional category and data
-Data = [[key, val] for key, val in mutations.items()]
-# replace some functional categories with a shorter name
-for i in range(len(Data)):
-    if Data[i][0] == 'stopgain':
-        Data[i][0] = 'PSC'
-    elif Data[i][0] == 'stoploss':
-        Data[i][0] = 'SCL'
-    elif Data[i][0] == 'non-synonymous':
-        Data[i][0] = 'NonSyn'
-    elif Data[i][0] == 'synonymous':
-        Data[i][0] = 'Syn'
-    # keep the same name for the other categories (tRNA, DLoop, NonCoding, Ribosomal)
-
-# sort according to names
-Data.sort()
-
-# create parallel lists with functional names and data
-categories = [Data[i][0] for i in range(len(Data))]
-data = [Data[i][1] for i in range(len(Data))]    
-print(categories)
-
-
-# create figure
+# create a figure
 fig = plt.figure(1, figsize = (3.5, 2.5))
 # add a plot to figure (1 row, 1 column, 1 plot)
-ax = fig.add_subplot(1, 1, 1)  
+ax = fig.add_subplot(1, 1, 1)
 
-# create a list to store the variables for building the legend
-Graphs = []
+# make a list with all data
+Data = [AA, AG, GG] 
 
-# make a list of colors
-colorscheme = ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5']
+# plot jittered data points 
+i = 0
+for j in range(len(Data)):
+    k = np.random.uniform(-0.2, 0.2, len(Data[j])) + (i+1)
+    plt.plot(k, Data[j], 'o', markersize = 2, markeredgecolor = 'black', markeredgewidth = 1.5, markerfacecolor = 'black', alpha = 0.5)
+    i += 1
 
-# loop over categories
-for i in range(len(categories)):
-    print(categories[i])
-    graph = ax.step(data[i], np.linspace(0, 1, len(data[i]), endpoint=True), linewidth = 1.2, color = colorscheme[i], alpha = 0.7)
-    Graphs.append(graph)
-print('plotted CDF')
+# limit x and y axis
+ax.set_ylim([0, 1])
+ax.set_xlim([0,4])
+  
+# set font for all text in figure
+FigFont = {'fontname':'Helvetica'}
 
-# add label for the Y axis
-ax.set_ylabel('Cumulative fraction of mutations', size = 10, ha = 'center', fontname = 'Arial')
+# set y axis label
+ax.set_ylabel('Alternative Allele Frequency', size = 10, ha = 'center', **FigFont)
+
+# add labels to x-ticks, rotate and align right, set size to 14
+ax.set_xticklabels(['A/A', 'A/G', 'G/G'], ha = 'center', size = 10, **FigFont)
+
 # set x axis label
-ax.set_xlabel('Mutant allele Frequency', size = 10, ha = 'center', fontname = 'Arial')
+ax.set_xlabel('rs11156878', size = 10, ha = 'center', **FigFont)
 
-# do not show lines around figure, keep bottow line  
+# do not show lines around figure  
 ax.spines["top"].set_visible(False)    
 ax.spines["bottom"].set_visible(True)    
 ax.spines["right"].set_visible(False)    
-ax.spines["left"].set_visible(False)      
+ax.spines["left"].set_visible(False)  
+
 # offset the spines
 for spine in ax.spines.values():
   spine.set_position(('outward', 5))
 
-# add a light grey horizontal grid to the plot, semi-transparent, 
-ax.yaxis.grid(True, linestyle='--', which='major', color='lightgrey', alpha=0.5, linewidth = 0.5)  
-# hide these grids behind plot objects
-ax.set_axisbelow(True)
-
-# do not show ticks on 1st graph
-ax.tick_params(
-    axis='x',       # changes apply to the x-axis and y-axis (other option : x, y)
-    which='both',      # both major and minor ticks are affected
-    bottom='on',      # ticks along the bottom edge are off
-    top='off',         # ticks along the top edge are off
-    right = 'off',
-    left = 'off',          
-    labelbottom='on', # labels along the bottom edge are off 
-    colors = 'black',
-    labelsize = 10,
-    direction = 'out') # ticks are outside the frame when bottom = 'on
-
 # do not show ticks
-ax.tick_params(
+plt.tick_params(
     axis='y',       # changes apply to the x-axis and y-axis (other option : x, y)
     which='both',      # both major and minor ticks are affected
     bottom='off',      # ticks along the bottom edge are off
@@ -321,147 +277,27 @@ ax.tick_params(
     left = 'off',          
     labelbottom='off', # labels along the bottom edge are off 
     colors = 'black',
-    labelsize = 10,
-    direction = 'out') # ticks are outside the frame when bottom = 'on
+    labelsize = 10)
+      
 
+# do not show ticks
+plt.tick_params(
+    axis='x',       # changes apply to the x-axis and y-axis (other option : x, y)
+    which='both',      # both major and minor ticks are affected
+    bottom='off',      # ticks along the bottom edge are off
+    top='off',         # ticks along the top edge are off
+    right = 'off',
+    left = 'off',          
+    labelbottom='on', # labels along the bottom edge are on 
+    colors = 'black',
+    labelsize = 10)
+
+
+# Set the tick labels font name
 for label in ax.get_yticklabels():
-    label.set_fontname('Arial')
-
-# add lines
-lns = Graphs[0]
-for i in range(1, len(Graphs)):
-    lns = lns + Graphs[i]
-# get labels
-labs = [categories[i] for i in range(len(categories))]
-print(labs)
-# plot legend
-ax.legend(lns, labs, loc=4, fontsize = 6, frameon = False)
-
-# build outputfile namewith parameters
-# extract the cancer name
-if which_files == 'singlefile' and sample == 'tumor':
-    cancer = HeteroSummaryFile[HeteroSummaryFile.index('_') + 1: HeteroSummaryFile.index('_' + sample)]
-elif which_files == 'singlefile' and sample == 'specific':
-    cancer = HeteroSummaryFile[HeteroSummaryFile.index('_') + 1: HeteroSummaryFile.index('_TumorSpecific')]
-
-if which_files == 'singlefile':
-    outputfile = 'CDFMutantAlleleFreq' + cancer + sample.capitalize() + '.pdf'
-elif which_files == 'allfiles':
-    outputfile = 'CDFMutantAllelefreq' + sample.capitalize() + '.pdf'
-
-fig.savefig(outputfile, bbox_inches = 'tight')
-
-
-
-
-
-
-
-#####################################
-
-
-
-
-# get inputfile from command
-genotype_file = sys.argv[1]
-# get outputfile
-figure_file = sys.argv[2]
-# get the ID of the SNP
-SNP_ID = sys.argv[3]
-
-# create a dict with {genotypes: list of allelic frequencies}
-genotypes = {}
-# open file for reading
-infile = open(genotype_file, 'r')
-#loop over file
-for line in infile:
-    line = line.rstrip()
-    if line != '':
-        line = line.split()
-        # get genotype
-        genotype = line[1]
-        # make a list of allelic frequencies
-        freq = line[2].split(';')
-        altfreq = list(map(lambda x: float(x), freq))
-        # check if genotype in dict
-        if genotype in genotypes:
-            # add all frequency values
-            genotypes[genotype].extend(altfreq)
-        else:
-            # add genotype : list of frequencies pairs to dict
-            genotypes[genotype] = altfreq
-# close file 
-infile.close()
-
-
-# make a list of genotypes
-SNP = [i for i in genotypes]
-# sort list
-SNP.sort()
-
-
-# create a figure
-fig = plt.figure(1, figsize = (5, 3))
-# add a plot to figure (1 row, 1 column, 1 plot)
-ax = fig.add_subplot(1, 1, 1)
-
-#for i in range(len(SNP)):
-#    for j in genotypes[SNP[i]]:
-#        ax.scatter(i+1, j, edgecolor = 'black', facecolor = 'black', lw = 1, s = 10, alpha = 0.5)
-
-
-
-xA = np.random.normal(1, 0.1, len(genotypes[SNP[0]])) 
-         
-
-xB = np.random.normal(2, 0.1, len(genotypes[SNP[1]]))
-
-xC = np.random.normal(3, 0.1, len(genotypes[SNP[2]]))
-
-
-plt.scatter(xA, genotypes[SNP[0]], edgecolor = 'black', facecolor = 'black', lw = 1, s = 10, alpha = 0.5)
-plt.scatter(xB, genotypes[SNP[1]], edgecolor = 'grey', facecolor = 'grey', lw = 1, s = 10, alpha = 0.5) 
-plt.scatter(xC, genotypes[SNP[2]], edgecolor = 'lightgrey', facecolor = 'lightgrey', lw = 1, s = 10, alpha = 0.5)
-
-
-ax.set_ylim([0, 1])
-ax.set_xlim([0,4])
-            
-# set y axis label
-ax.set_ylabel('Alternative Allele Frequency', size = 10, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
-
-# add labels to x-ticks, rotate and align right, set size to 14
-ax.set_xticklabels([SNP[0], SNP[1], SNP[2]], ha = 'center', size = 10, fontname = 'Helvetica', family = 'sans-serif')
-
-# set x axis label
-ax.set_xlabel(SNP_ID, size = 10, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
-
-# remove top axes and right axes ticks
-ax.get_xaxis().tick_bottom()
-ax.get_yaxis().tick_left()
-
-
-# set positions of the x-axis ticks (center of the bars as bar labels)
-tick_pos = [i+1 for i in range(3)]
-
-plt.xticks(tick_pos, SNP)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    label.set_fontname('Helvetica')
+    
 
 # save figure
-fig.savefig(figure_file, bbox_inches = 'tight')
-
-
-
+fig.savefig('truc.pdf', bbox_inches = 'tight')
 
